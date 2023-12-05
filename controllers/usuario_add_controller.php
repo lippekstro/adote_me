cebf49222920d469c7fa2acdd7cbab1cf11c96c0
marcolaboiafria <marcosifma123@gmail.com>
2023-12-05 11:21:40 -0300

    criacao de controler usuario(marcola)

---
+++ controllers/usuario_add_controller.php
@@ -0,0 +1,19 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/adote_me/models/usuarios.php';
+
+try {
+    $pergunta = $_POST['pergunta'];
+    $resp = $_POST['resposta'];
+
+    $faq = new Faq();
+    $faq->faq_pergunta = $pergunta;
+    $faq->faq_resposta = $resp;
+
+    $faq->criar();
+
+    header('Location: /adote_me/views/admin/faqs_listar.php');
+    exit();
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
